4eb9534d2c4b2038c780a9d3ffc9db83904c17d3
Benjamin Kniffler <bkniffler@me.com>
2016-02-27 17:59:31 +0100

    No more CSS via webpack

--- example/webpack.js
+++ example/webpack.js
@@ -1,28 +1,19 @@
 var path = require('path');
 var ExtractTextPlugin = require("extract-text-webpack-plugin-webpack-2");
 
-var debug = process.env.NODE_ENV === 'development', production = !debug, plugins = [];
-
-if(production){
-    plugins.push(new ExtractTextPlugin('../css/bundle.css'));
-}
+var debug = process.env.NODE_ENV === 'development', production = !debug;
 
 module.exports = {
     cache: debug,
     debug: debug,
     devtool: production ? 'eval' : null,
     entry: path.resolve(__dirname, 'app.js'),
-    plugins: plugins,
     module: {
         loaders: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: 'babel',
-            },
-            {
-                test: /\.css$/,
-                loader: debug ? "style-loader!css-loader" : ExtractTextPlugin.extract("style-loader", "css-loader")
             }
         ]
     },
